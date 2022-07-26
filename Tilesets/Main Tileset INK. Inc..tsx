39227b95d0df7f74f765c1808204c004b13188d1
salt-bag <72279341+salt-bag@users.noreply.github.com>
2022-07-26 11:00:54 -0500

    Add files via upload

---
+++ Tilesets/Main Tileset INK. Inc..tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Main Tileset INK. Inc." tilewidth="16" tileheight="16" tilecount="24" columns="4">
+ <image source="Tileset.png" width="64" height="96"/>
+ <tile id="2"/>
+ <tile id="3"/>
+ <tile id="6"/>
+ <tile id="9"/>
+ <tile id="10"/>
+ <tile id="0"/>
+ <tile id="14"/>
+ <tile id="12"/>
+ <tile id="13"/>
+ <tile id="22"/>
+ <tile id="5"/>
+ <tile id="16"/>
+ <tile id="17"/>
+ <tile id="21"/>
+ <tile id="8"/>
+ <tile id="4"/>
+ <tile id="20"/>
+ <tile id="18"/>
+ <tile id="1"/>
+ <tile id="11"/>
+ <tile id="15"/>
+ <tile id="19"/>
+ <tile id="23"/>
+ <tile id="7"/>
+</tileset>
